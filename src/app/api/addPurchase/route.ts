bb8d371910d38c6fd3be585c864a417de0a228a1
Hazim Halim <hazim.halim54@yahoo.com>
2024-12-24 04:02:49 +0800

    ensuring records are added chronologically. code cleanup

--- src/app/api/addPurchase/route.ts
+++ src/app/api/addPurchase/route.ts
@@ -7,27 +7,59 @@ export async function POST(request: Request) {
   try {
     await client.connect()
     const database = client.db("bukku")
-    const purchase = database.collection("purchase")
-    const sale = database.collection("sale")
     const inventory = database.collection("inventory")
-    const documentBody = {
-      date: result.date,
-      quantity: result.quantity,
-      price: result.price,
-      totalValue: result.price * result.quantity
+    const transactions = database.collection("transactions")
+
+    let documentBody = {}
+    
+    // Bodies have different fields based on transaction types
+    if(result.transactionType == 'purchase'){
+      documentBody = {
+        date: result.date,
+        quantity: result.quantity,
+        price: result.price,
+        totalValue: result.price * result.quantity,
+        transactionType: result.transactionType
+      }
+    }else if(result.transactionType =='sale'){
+      documentBody = {
+        date: result.date,
+        quantity: result.quantity,
+        price: result.price,
+        totalAmount: result.totalAmount,
+        totalCost: result.totalCost,
+        transactionType: result.transactionType
+      }
     }
     
     
     // Check if there's already a document with the same date. Only one transaction is alowed per day
-    const purchaseDoc = await purchase.findOne({"date": result.date})
-    const saleDoc = await sale.findOne({"date": result.date})
-    if (purchaseDoc || saleDoc){
+    const transactionDoc = await transactions.findOne({"date": result.date})
+    if (transactionDoc){
       return Response.json({status: 400, message: "Transaction already made today"})
     }else{
-      const res = await purchase.insertOne(documentBody)
-      await inventory.findOneAndUpdate({},{
-        $inc: { totalGoods: result.quantity, totalValue: Number((result.price * result.quantity).toFixed(2)) },
+
+      // Getting the most recent transaction to make sure transactions are made in chronological order
+      const recentTransaction = await transactions.findOne({}, {
+        sort: { "date" : -1 }
       })
+      if( recentTransaction && (new Date(result.date) < new Date(recentTransaction.date))){
+        return Response.json({status: 400, message: `New transactions needs to be made after ${recentTransaction.date}`})
+      }
+
+      // Inserting new transaction records
+      const res = await transactions.insertOne(documentBody)
+
+      // Updating Total Goods and Total Value 
+      if (result.transactionType == 'purchase'){
+        await inventory.findOneAndUpdate({},{
+          $inc: { totalGoods: result.quantity, totalValue: Number((result.price * result.quantity).toFixed(2)) },
+        })
+      }else{
+        await inventory.findOneAndUpdate({},{
+          $inc: { totalGoods: -result.quantity, totalValue: -Number((result.totalCost).toFixed(2)) },
+        })
+      }
       return Response.json(res);
     }
   }catch(error){
@@ -42,10 +74,14 @@ export async function GET(request: Request) {
   try {
     await client.connect()
     const db = client.db("bukku")
+    const transactions = db.collection("transactions")
+
+    // Separating purchase and sale transactions into 2 different arrays for ease of use
+    const purchase = await transactions.find({"transactionType": "purchase"}).toArray();
+    const sale = await transactions.find({"transactionType": "sale"}).toArray();
     
-    const purchase = db.collection("purchase")
-    const data = await purchase.find({}).toArray();
-    return Response.json({data})
+    return Response.json({"purchases" : purchase, "sales": sale})
+
   }catch(err){
     return Response.json({status: 500, message: "ha?"})
   }
